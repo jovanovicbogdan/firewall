63164b17143f669fc6545331bd42158989e2c4ba
Bogdan Jovanovic <jbogdan1998@hotmail.com>
2026-04-26 16:20:22 +0200

    test: cleanup

--- src/test/java/dev/bogdanjovanovic/firewall/rest/FirewallIntegrationTests.java
+++ src/test/java/dev/bogdanjovanovic/firewall/rest/FirewallIntegrationTests.java
@@ -9,9 +9,6 @@ import org.springframework.http.MediaType;
 public class FirewallIntegrationTests extends BaseTest {
 
   private static final String FIREWALL_RULES_API = "/api/v1/firewall/rules";
-  private static final String FIREWALL_DECISION_API = "/api/v1/firewall/decision";
-  private static final String DECISION_QUERY_PARAM_SRC_IP = "srcIp";
-  private static final String DECISION_QUERY_PARAM_DEST_IP = "destIp";
 
   @Test
   void shouldAddNewRule() {
@@ -25,69 +22,6 @@ public class FirewallIntegrationTests extends BaseTest {
         "35.35.35.35",
         Action.ALLOW.name()
     ))
-        .exchange()
-        .expectStatus().is2xxSuccessful();
-  }
-
-  @Test
-  void shouldAllowWhenSrcIpAndDestIpInRange() {
-    final var addRuleRequest = new AddRuleRequest(
-        "1.1.1.1",
-        "10.10.10.10",
-        "10.10.10.10",
-        "15.15.15.15",
-        Action.ALLOW.name()
-    );
-
-    addRule(addRuleRequest);
-
-    sleep(firewallConfig.cooldownMs() + 500L);
-
-    final var srcIp = addRuleRequest.getSrcStart();
-    final var destIp = addRuleRequest.getDestStart();
-
-    restTestClient.get()
-        .uri(uriBuilder -> uriBuilder
-            .path(FIREWALL_DECISION_API)
-            .queryParam(DECISION_QUERY_PARAM_SRC_IP, srcIp)
-            .queryParam(DECISION_QUERY_PARAM_DEST_IP, destIp)
-            .build())
-        .exchange()
-        .expectStatus().is2xxSuccessful();
-  }
-
-  @Test
-  void shouldNotAllowWhenSrcIpAndDestIpInRangeAndActionDeny() {
-    final var addRuleRequest = new AddRuleRequest(
-        "60.60.60.60",
-        "70.70.70.70",
-        "80.80.80.80",
-        "90.90.90.90",
-        Action.DENY.name()
-    );
-
-    addRule(addRuleRequest);
-
-    sleep(firewallConfig.cooldownMs() + 500L);
-
-    final var srcIp = addRuleRequest.getSrcStart();
-    final var destIp = addRuleRequest.getDestStart();
-
-    restTestClient.get()
-        .uri(uriBuilder -> uriBuilder
-            .path(FIREWALL_DECISION_API)
-            .queryParam(DECISION_QUERY_PARAM_SRC_IP, srcIp)
-            .queryParam(DECISION_QUERY_PARAM_DEST_IP, destIp)
-            .build())
-        .exchange()
-        .expectStatus().is4xxClientError();
-  }
-
-  private void addRule(final AddRuleRequest request) {
-    restTestClient.post()
-        .uri(FIREWALL_RULES_API)
-        .contentType(MediaType.APPLICATION_JSON)
-        .body(request)
         .exchange()
         .expectStatus().is2xxSuccessful();
   }
