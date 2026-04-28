d3f2d4f5c70c68ac2f593b4af71fb6cb15330858
Bogdan Jovanovic <jbogdan1998@hotmail.com>
2026-04-28 17:06:55 +0200

    fix: PgQ listener connection leak

--- src/main/java/dev/bogdanjovanovic/firewall/infrastructure/sync/PgQListener.java
+++ src/main/java/dev/bogdanjovanovic/firewall/infrastructure/sync/PgQListener.java
@@ -2,6 +2,7 @@ package dev.bogdanjovanovic.firewall.infrastructure.sync;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,25 +11,37 @@ public class PgQListener implements Runnable {
 
   private static final String CHANNEL = "rule_events";
   private static Connection CONNECTION = null;
+
+  private final AtomicBoolean listenerRegistered = new AtomicBoolean(false);
   private final DataSource dataSource;
 
   public PgQListener(final DataSource dataSource) {
     this.dataSource = dataSource;
+    init();
   }
 
   @Override
   public void run() {
-    if (CONNECTION == null) {
-      establishConnection();
-      return;
+    if (!listenerRegistered.get()) {
+      registerListener();
     }
 
     try (final var stmt = CONNECTION.createStatement()) {
       stmt.execute("SELECT 1;");
     } catch (Exception ex) {
       log.error("PgQ listener failed", ex);
-      if (ex instanceof SQLException) {
-        validateConnection();
+      try {
+        if (CONNECTION.isValid(3)) return;
+
+        CONNECTION.close();
+        CONNECTION = null;
+        listenerRegistered.set(false);
+
+        if (establishConnection()) {
+          registerListener();
+        }
+      } catch (SQLException sqlException) {
+        log.error("Failed to close the connection", sqlException);
       }
     }
   }
@@ -37,14 +50,21 @@ public class PgQListener implements Runnable {
     return CONNECTION;
   }
 
-  private void establishConnection() {
+  private void init() {
+    if (establishConnection()) {
+      registerListener();
+    }
+  }
+
+  private boolean establishConnection() {
     try {
       log.info("Attempting to establish a connection");
       CONNECTION = dataSource.getConnection();
-      registerListener();
+      log.info("PgQ listener connection established");
+      return true;
     } catch (Exception ex) {
       log.error("Failed to establish a connection to PgQ", ex);
-      CONNECTION = null;
+      return false;
     }
   }
 
@@ -52,20 +72,10 @@ public class PgQListener implements Runnable {
     try (final var stmt = getConnection().createStatement()) {
       stmt.execute(String.format("LISTEN %s", CHANNEL));
       log.info("Listening on PgQ channel {}", CHANNEL);
+      listenerRegistered.set(true);
     } catch (Exception ex) {
-      log.error("Failed to register PgQ listener", ex);
-    }
-  }
-
-  private void validateConnection() {
-    try {
-      if (!CONNECTION.isValid(3)) {
-        CONNECTION.close();
-        CONNECTION = null;
-      }
-    } catch (Exception ex) {
-      log.info("Connection is invalid", ex);
-      CONNECTION = null;
+      log.error("Failed to register a listener", ex);
+      listenerRegistered.set(false);
     }
   }
 
